a8ad56bf1991304ee6f4052497c5e9a3f719bdb8
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-09-03 20:37:42 +0600

    object added

---
+++ Object/property.js
@@ -0,0 +1,27 @@
+
+// jokhon object er sokol property and value dekhte cai 
+const person = {
+    name : " Samir Uddin",
+    age : 18,
+    profession : 'Developer',
+    Sellary : 2500,
+    Married : true
+}
+
+console.log(person);
+
+
+// jokhon kono object er direct kono property decte cai
+// dot notation diye object er property access kora
+
+console.log(person.Married);
+// or 
+const sadi = person.Married;
+console.log( sadi);
+
+
+// third bracket diye access kora 
+console.log(person['age'])
+// or
+const boyos = person['age'];
+console.log(boyos)
